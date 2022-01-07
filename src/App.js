a5cb3a81677328d02dc92dcb728ff433e8d30341
harsh-sharma-99 <sharmaharsh9944@gmail.com>
2022-01-07 21:19:05 +0530

    added topfold layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./components/header";
 import Home from "./pages/home";
+import Footer from "./components/footer/index";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
-      <div>Footer</div>
+      <Footer />
     </div>
   );
 }
--- src/components/topfold/index.js
+++ src/components/topfold/index.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 
 const TopFold = () => {
+  const [query, setQuery] = useState("");
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+  };
   return (
     <div className="topfold-container">
       <div className="search-bar">
-        {/* <input
-          type="search"
-          placeholder="Search List"
-          className="input-style"
-          results
-        /> */}
+        <input placeholder="Search" onChange={handleQuery} value={query} />
       </div>
-      <div className="add-button">Add</div>
+      <div className="add-button">
+        <button className="add-button-style">Add New List</button>
+      </div>
     </div>
   );
 };
